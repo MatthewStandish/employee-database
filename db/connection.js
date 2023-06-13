4004dcfdb274a8c78a52068e5e1cb3e957bdea4f
Matthew Standish <mwstandish@aol.com>
2023-06-13 19:55:12 -0400

    added connection

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const connection = mysql.createConnection({
+  host: "process.env.host",
+  port: 3306,
+  user: "process.env.username",
+  password: "process.env.password",
+  database: "process.env.database",
+});
+connection.connect();
+connection.query = util.promisify(connection.query);
+module.exports = connection;
